ca95e778ac23cbdbb4d59dbd3803250d2eacf405
manoloudios-proggramatistis <ekpliktikapragmata@gmail.com>
2024-07-02 14:00:51 +0300

    Add files via upload

---
+++ helloworld.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      //Output  to  the console greetings to the  world
+      print("hello,world!");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
